96c5ed2113b085c0dfbffd09926e4ef607b2ae43
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-23 13:45:49 -0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,15 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:30:56 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/23 13:33:02 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/23 13:45:25 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
-
-
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 
 #endif
