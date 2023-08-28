583c21f93f3a5b61b259c291f88b352e333f59b3
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-28 12:05:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,18 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "includes/ft_printf.h"
 #include <stdio.h>
+
+void	test1(void)
+{
+	int	my_return;
+	int	original_return;
+
+	char	c = 'c';
+	char	v = 'v';
+	char	b = 'b';
+
+	my_return = ft_printf("Muitas %c %c %c Coisas\n", c, v, b);  // My Function
+	printf("my return: %d\n\n", my_return);
+
+	original_return = printf("Muitas %c %c %c Coisas\n", c, v, b);  // Original
+	printf("original return: %d\n", original_return);
+}
+
+void	test2(void)
+{
+	int	my_return;
+	int	original_return;
+
+	char	*str1 = "boas";
+	char	*str2 = "legais";
+
+	my_return = ft_printf("Muitas %s %s %s Coisas\n", str1, str2, str1);  // My Function
+	printf("my return: %d\n\n", my_return);
+
+	original_return = printf("Muitas %s %s %s Coisas\n", str1, str2, str1);  // Original
+	printf("original return: %d\n", original_return);
+}
 
 int main(void)
 {
-	ft_printf("%d %s %c", 99, "bahhhh", 'v');
-	printf("\n");
-	ft_printf("%c %c", 'a', 'b');
-	printf("\n");
-	ft_printf("%s %s", "coisa", "baaa");
-	printf("\n");
-	ft_putstr("Fim dos testes :)\n");
-	ft_putnbr_fd(12, 1);
+	printf("========== Test 01 ==========\n");
+	test1();
+	printf("\n========== Test 02 ==========\n");
+	test2();
 	return (0);
 }
