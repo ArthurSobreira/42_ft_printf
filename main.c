42a0a7eaf27d529a12ed210bb09aefb13b214799
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-28 18:59:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -44,11 +44,42 @@ void	test2(void)
 	printf("original return: %d\n", original_return);
 }
 
+void	test3(void)
+{
+	int	my_return;
+	int	original_return;
+
+	char	x = 'X';
+	char	*str1 = "Men";
+
+	my_return = ft_printf("%c-%s Revolution\n", x, str1);  // My Function
+	printf("my return: %d\n\n", my_return);
+
+	original_return = printf("%c-%s Revolution\n", x, str1);  // Original
+	printf("original return: %d\n", original_return);
+}
+
+void	test4(void)
+{
+	int	my_return;
+	int	original_return;
+
+	my_return = ft_printf("%% %% %% %%\n");  // My Function
+	printf("my return: %d\n\n", my_return);
+
+	original_return = printf("%% %% %% %%\n");  // Original
+	printf("original return: %d\n", original_return);
+}
+
 int main(void)
 {
 	printf("========== Test 01 ==========\n");
 	test1();
 	printf("\n========== Test 02 ==========\n");
 	test2();
+	printf("\n========== Test 03 ==========\n");
+	test3();
+	printf("\n========== Test 04 ==========\n");
+	test4();
 	return (0);
 }
