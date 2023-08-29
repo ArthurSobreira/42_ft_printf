3b7df03b6eb34431343c116f8895b71271bb01a2
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-29 09:58:32 -0300

    Update printf header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:30:56 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/24 16:38:31 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/28 20:04:53 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,13 @@
 // Include lib header
 # include "libft.h"
 
+// Macros Definitions
+# define DECIMAL_BASE "0123456789"
+
 // Functions Prototypes
-int		ft_printf(const char *format, ...);
-int		ft_putchar(char c);
-int		ft_putstr(char *str);
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putnbr_base(long int number, char *base);
+int	ft_putstr(char *str);
 
 #endif
