d25434f3785d30d7ecfa54c79b4ec9fbf8f7b8ec
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-29 20:13:56 -0300

    Update ft_putptr.c

--- src/ft_putptr.c
+++ src/ft_putptr.c
@@ -6,14 +6,29 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:45:11 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/29 19:49:22 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/29 20:10:09 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putptr(unsigned long number)
+int	ft_putptr(unsigned long number, char *base)
 {
-	(void)number;
-	return (0);	
+	int	lenght;
+	int	base_lenght;
+
+	lenght = 0;
+	base_lenght = ft_strlen(base);
+	if (number == 0)
+	{
+		return (ft_putstr("(nil)"));
+	}
+	if (number >= (unsigned long)base_lenght)
+	{
+		lenght += ft_putptr((number / base_lenght), base);
+	}
+	else
+		lenght += ft_putstr("0x");
+	lenght += ft_putchar(base[number % base_lenght]);
+	return (lenght);	
 }
