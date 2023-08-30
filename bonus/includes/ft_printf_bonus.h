0bb3ddb10cd4d87ea284629e1ea92457feed8e26
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-30 20:10:56 -0300

    Update bonus header

--- bonus/includes/ft_printf_bonus.h
+++ bonus/includes/ft_printf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:30:56 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/30 14:49:05 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/30 20:05:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@
 int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putnbr_base(long int number, char *base);
+int	ft_puthexa_low(long int number, char flag);
 int	ft_putptr(unsigned long number, char *base);
 int	ft_putstr(char *str);
 
