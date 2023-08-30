912e4e6b8c11d2cc682e5369e22920a194c02168
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-30 14:52:51 -0300

    Update Mandatory folder

---
+++ mandatory/includes/ft_printf.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 13:30:56 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/08/30 11:40:12 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+// Include lib header
+# include "../../libft/includes/libft.h"
+
+// Macros Definitions
+# define DECIMAL_BASE "0123456789"
+# define HEXA_LOW_BASE "0123456789abcdef"
+# define HEXA_UP_BASE "0123456789ABCDEF"
+
+// Functions Prototypes
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putnbr_base(long int number, char *base);
+int	ft_putptr(unsigned long number, char *base);
+int	ft_putstr(char *str);
+
+#endif
