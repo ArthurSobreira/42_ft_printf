2989ddd325b2d9f51add7483644c1070cba933be
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-30 20:11:35 -0300

    Update ft_printf_bonus.c

--- bonus/src/ft_printf_bonus.c
+++ bonus/src/ft_printf_bonus.c
@@ -6,19 +6,19 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 11:13:26 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/30 14:54:59 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/30 20:08:20 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
+#include <stdio.h> // DELETA ISSO
 
-static int	ft_check_format_specifier(va_list args, char current_format);
+static int	ft_check_format_specifier(va_list args, char current_format, char flag);
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	size_t	index;
-	char	current_format;
 	int		lenght;
 
 	if (format == NULL)
@@ -31,8 +31,9 @@ int	ft_printf(const char *format, ...)
 		if (format[index] == '%')
 		{
 			index++;
-			current_format = format[index];
-			lenght += ft_check_format_specifier(args, current_format);
+			while (format[index] == '#' || format[index] == '+' || format[index] == ' ')
+				index++;
+			lenght += ft_check_format_specifier(args, format[index], format[index - 1]);
 		}
 		else
 			lenght += ft_putchar(format[index]);
@@ -42,11 +43,12 @@ int	ft_printf(const char *format, ...)
 	return (lenght);
 }
 
-static int	ft_check_format_specifier(va_list args, char current_format)
+static int	ft_check_format_specifier(va_list args, char current_format, char flag)
 {
-	size_t	count;
+	int	count;
 
 	count = 0;
+	(void)flag;
 	if (current_format == 'c')
 		count += ft_putchar((char)va_arg(args, int));
 	else if (current_format == 's')
@@ -57,10 +59,15 @@ static int	ft_check_format_specifier(va_list args, char current_format)
 		count += ft_putnbr_base(va_arg(args, int), DECIMAL_BASE);
 	else if (current_format == 'u')
 		count += ft_putnbr_base(va_arg(args, unsigned int), DECIMAL_BASE);
+		
+		
 	else if (current_format == 'x')
-		count += ft_putnbr_base(va_arg(args, unsigned int), HEXA_LOW_BASE);
+		count += ft_puthexa_low(va_arg(args, unsigned int), flag);
+		
 	else if (current_format == 'X')
 		count += ft_putnbr_base(va_arg(args, unsigned int), HEXA_UP_BASE);
+
+		
 	else if (current_format == '%')
 		count += ft_putchar('%');
 	return (count);
