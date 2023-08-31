58ce4e77d574abd882fd2ed8c47660edc58ad7e2
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-31 14:57:15 -0300

    Update ft_printf_bonus.h

--- bonus/includes/ft_printf_bonus.h
+++ bonus/includes/ft_printf_bonus.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:30:56 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/31 11:12:16 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/31 14:44:15 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@
 // Functions Prototypes
 int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
-int	ft_putnbr_base(long int number, char *base);
 int	ft_puthexa(long int number, char *base, char flag);
+int	ft_putnbr_base(long int number, char *base);
+int	ft_putplus(long int number, char *base, char flag);
 int	ft_putptr(unsigned long number, char *base);
 int	ft_putstr(char *str);
 
