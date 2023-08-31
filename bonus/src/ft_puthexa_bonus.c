530053450ab3be3df112b7cb046a5992d1c3d271
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-31 11:20:00 -0300

    Rename file

--- bonus/src/ft_puthexa_bonus.c
+++ bonus/src/ft_puthexa_bonus.c
@@ -6,14 +6,13 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 19:16:54 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/30 20:06:02 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/31 11:10:47 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
-#include <stdio.h>
 
-int	ft_puthexa_low(long int number, char flag)
+int	ft_puthexa(long int number, char *base, char flag)
 {
 	int	lenght;
 
@@ -22,6 +21,6 @@ int	ft_puthexa_low(long int number, char flag)
 	{
 		lenght += ft_putstr("0x");
 	}
-	lenght += ft_putnbr_base(number, HEXA_LOW_BASE);
+	lenght += ft_putnbr_base(number, base);
 	return (lenght);
 }
