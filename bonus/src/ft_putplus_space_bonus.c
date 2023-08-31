4265072c54c75856bf6e7120f27235f1a302be76
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-31 15:27:58 -0300

    Rename file

--- bonus/src/ft_putplus_space_bonus.c
+++ bonus/src/ft_putplus_space_bonus.c
@@ -6,13 +6,13 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 14:38:05 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/31 14:51:25 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/31 15:21:28 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_bonus.h"
 
-int	ft_putplus(long int number, char *base, char flag)
+int	ft_putplus_space(long int number, char *base, char flag)
 {
 	int	length;
 
