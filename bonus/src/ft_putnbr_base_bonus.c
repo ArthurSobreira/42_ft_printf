c541a112a28345a8bc2df4a9692599cff0cdcfc3
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-31 11:50:08 -0300

    Update ft_putnbr_base_bonus.c

--- bonus/src/ft_putnbr_base_bonus.c
+++ bonus/src/ft_putnbr_base_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 19:09:59 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/08/30 14:55:22 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/31 11:46:57 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 int	ft_putnbr_base(long int number, char *base)
 {
-	int	lenght;
+	int	length;
 	int	base_lenght;
 
-	lenght = 0;
+	length = 0;
 	base_lenght = ft_strlen(base);
 	if (number == -2147483648)
 	{
-		lenght += ft_putchar('-');
-		lenght += ft_putchar('2');
+		length += ft_putchar('-');
+		length += ft_putchar('2');
 		number = 147483648;
 	}
 	if (number < 0)
 	{
-		lenght += ft_putchar('-');
+		length += ft_putchar('-');
 		number *= -1;
 	}
 	if (number >= base_lenght)
 	{
-		lenght += ft_putnbr_base((number / base_lenght), base);
+		length += ft_putnbr_base((number / base_lenght), base);
 	}
-	lenght += ft_putchar(base[number % base_lenght]);
-	return (lenght);
+	length += ft_putchar(base[number % base_lenght]);
+	return (length);
 }
